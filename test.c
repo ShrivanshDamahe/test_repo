8b9deae3eec71c32bd53f8a8911be5256c3ea54b
ShrivanshDamahe <140092918+ShrivanshDamahe@users.noreply.github.com>
2024-12-16 21:49:59 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,16 +1,5 @@
-    static int decimalToBinary(int N)
+    static int firstFunction(int N)
     {
-
-        int ans = 0;
-        int cnt = 0;
-
-        while (N != 0) {
-            int rem = N % 2;
-            N /= 2;
-            ans += rem * Math.pow(10, cnt);
-
-            cnt++;
-        }
-
-        return ans;
+        printf("Hello World!!!");
+        return 0;
     }
